f438ca4ffd9f8e0becaffb3d71c0dff967a28f05
El dano <danielpliego@gmail.com>
2021-08-14 13:27:32 -0500

    fix redes

--- src/components/modal-bruja.jsx
+++ src/components/modal-bruja.jsx
@@ -10,12 +10,12 @@ export function ModalBruja() {
       <h1 className="text-light text-shadow display mb-4">
         Follow the gaang!
       </h1>
-      <a href="https://instagram.com/@pachefestmx " target="_blank" className="sativa-btn d-block mb-3">@pachefestmx</a>
-      <a href="https://instagram.com/@highgaang " target="_blank" className="sativa-btn d-block mb-3 btn-purple">@highgaang</a>
-      <a href="https://instagram.com/@escueladecocinacannabica" target="_blank" className="sativa-btn d-block mb-3 btn-orange font-xs">@escueladecocinacannabica</a>
-      <a href="https://instagram.com/@holafilomena_" target="_blank" className="sativa-btn d-block mb-3 btn-blue">@holafilomena_</a>
-      <a href="https://instagram.com/@chicksvsstigma " target="_blank" className="sativa-btn btn-yellow d-block mb-3">@chicksvsstigma</a>
-      <a href="https://instagram.com/@incendiarias__" target="_blank" className="sativa-btn btn-purple d-block mb-3 ">@incendiarias__</a>
+      <a href="https://instagram.com/pachefestmx " target="_blank" className="sativa-btn d-block mb-3">@pachefestmx</a>
+      <a href="https://instagram.com/highgaang " target="_blank" className="sativa-btn d-block mb-3 btn-purple">@highgaang</a>
+      <a href="https://instagram.com/escueladecocinacannabica" target="_blank" className="sativa-btn d-block mb-3 btn-orange font-xs">@escueladecocinacannabica</a>
+      <a href="https://instagram.com/holafilomena_" target="_blank" className="sativa-btn d-block mb-3 btn-blue">@holafilomena_</a>
+      <a href="https://instagram.com/chicksvsstigma " target="_blank" className="sativa-btn btn-yellow d-block mb-3">@chicksvsstigma</a>
+      <a href="https://instagram.com/incendiarias__" target="_blank" className="sativa-btn btn-purple d-block mb-3 ">@incendiarias__</a>
       <a href="#" onClick={() => setOpenModal(false)} className="sativa-btn d-block mb-3 btn-text">Continuar a sativamx.com</a>
     </div>
   )

7512bd77dbb7a1bdfc99b779eb7e45f8da9aa5e7
El dano <danielpliego@gmail.com>
2021-08-14 13:27:50 -0500

    fix modal menu

--- src/components/modal-menu.jsx
+++ src/components/modal-menu.jsx
@@ -11,7 +11,7 @@ export function ModalMenu({ openModal, setOpenModal }) {
       <a href="/" className="sativa-btn d-block mb-3">Inicio</a>
       <a href="/search/?s=BEST_SELLING" className="sativa-btn btn-purple d-block mb-3">Tienda</a>
       <a href="https://www.pachefest.com" className="sativa-btn btn-yellow d-block mb-3">Pachefest</a>
-      <a href="https://instagram.com/@highgaang " target="_blank" className="sativa-btn btn-blue d-block mb-3 btn-purple">@highgaang</a>
+      <a href="https://instagram.com/highgaang " target="_blank" className="sativa-btn btn-blue d-block mb-3 btn-purple">@highgaang</a>
     </div>
   )
 }

cfbb88fdd4dddd740a4bada99531467ea70b34c1
Daniel Pliego <danielpliego@gmail.com>
2022-02-17 17:01:29 -0600

    change checkout button

--- src/pages/carrito.jsx
+++ src/pages/carrito.jsx
@@ -48,7 +48,7 @@ export default function CartPage() {
               Al parecer tu carrito está vacío. Sabemos que a veces es muy
               complicado elegir, tal vez esto puede ayudar:
             </p>
-            <Link to={homepageUrl} className="sativa-btn">
+            <Link to="/search?s=BEST_SELLING" className="sativa-btn">
               Ver best sellers
             </Link>
           </div>
@@ -124,7 +124,7 @@ export default function CartPage() {
               disabled={loading}
               className={checkoutButton}
             >
-              Checkout
+              Ir a pagar
             </button>
           </>
         )}

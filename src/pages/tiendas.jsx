eaabb418c16f7802e6c52bed24c86b7054a6d34f
Daniel Pliego <danielpliego@gmail.com>
2022-06-07 14:39:32 -0500

    add tiendas a pagina

--- src/pages/tiendas.jsx
+++ src/pages/tiendas.jsx
@@ -3,6 +3,9 @@ import { Layout } from "../components/layout"
 import Hipnosis from "../images/tiendas/hipnosis.jpg"
 import Incendiarias from "../images/tiendas/incendiarias.jpg"
 import ChicksVsStigma from "../images/tiendas/chicks.png"
+import Les from "../images/tiendas/les.jpg"
+import Mar from "../images/tiendas/mar.jpg"
+import Oxen from "../images/tiendas/oxen.jpg"
 import { homepageUrl } from "."
 
 export default function Tiendas() {
@@ -16,14 +19,13 @@ export default function Tiendas() {
     >
       <div className="section pb-4">
         <div className="container">
-          <h1 className="mb-5">Descúbrenos en estas tiendas</h1>
+          <h1 className="mb-5">Encuentra nuestros productos en las tiendas distribuidoras</h1>
           <div className="row mb-5">
             <div className="col-lg-4">
               <img src={Incendiarias} className="img-fluid mb-3" />
-              <h3 className="mb-2">Incendiarias</h3>
+              <h3 className="mb-2">Incendiarias - CDMX</h3>
               <p className="font-sm mb-3">
-                Calle Marsella 60, Juárez, Cuauhtémoc, 06030 Ciudad de México,
-                CDMX
+                Calle Marsella 60, Juárez
               </p>
               <a
                 href="https://www.instagram.com/incendiarias__/"
@@ -35,9 +37,9 @@ export default function Tiendas() {
             </div>
             <div className="col-lg-4">
               <img src={ChicksVsStigma} className="img-fluid mb-3" />
-              <h3 className="mb-2">Chiks vs stigma</h3>
+              <h3 className="mb-2">Chiks vs stigma - CDMX</h3>
               <p className="font-sm mb-3">
-                Av Nuevo León 284-local B, Hipódromo, Cuauhtémoc, 06170, CDMX
+                Av Nuevo León 284-local B, Hipódromo
               </p>
               <a
                 href="https://www.instagram.com/chicksvsstigma"
@@ -49,9 +51,9 @@ export default function Tiendas() {
             </div>
             <div className="col-lg-4">
               <img src={Hipnosis} className="img-fluid mb-3" />
-              <h3 className="mb-2">Tienda Hipnosis</h3>
+              <h3 className="mb-2">Tienda Hipnosis - CDMX</h3>
               <p className="font-sm mb-3">
-                Zacatecas 39, Roma Norte, Cuauhtémoc, 06700, CDMX
+                Zacatecas 39, Roma Norte
               </p>
               <a
                 href="https://www.instagram.com/hipnosismx/"
@@ -59,6 +61,50 @@ export default function Tiendas() {
                 className="btn d-inline-block"
               >
                 @HIPNOSISMX
+              </a>
+            </div>
+          </div>
+          <div className="row mb-5">
+            <div className="col-lg-4">
+              <img src={Les} className="img-fluid mb-3" />
+              <h3 className="mb-2">Le Sisteret’s - CDMX</h3>
+              <p className="font-sm mb-3">
+                Barrio Alameda, local #33
+              </p>
+              <a
+                href="https://www.instagram.com/le_sisterets/"
+                target="_blank"
+                className="btn d-inline-block"
+              >
+                @LE_SISTERETS
+              </a>
+            </div>
+            <div className="col-lg-4">
+              <img src={Oxen} className="img-fluid mb-3" />
+              <h3 className="mb-2">Oxen Concept Store - Guadalajara</h3>
+              <p className="font-sm mb-3">
+                Manuel López Cotilla #1370
+              </p>
+              <a
+                href="https://www.instagram.com/oxenconcept/"
+                target="_blank"
+                className="btn d-inline-block"
+              >
+                @OXENCONCEPT
+              </a>
+            </div>
+            <div className="col-lg-4">
+              <img src={Mar} className="img-fluid mb-3" />
+              <h3 className="mb-2">Mar Mexa - Cholula, Puebla</h3>
+              <p className="font-sm mb-3">
+                Calle 10 Oriente #604
+              </p>
+              <a
+                href="https://www.instagram.com/mar.mexa/"
+                target="_blank"
+                className="btn d-inline-block"
+              >
+                @MAR.MEXA
               </a>
             </div>
           </div>

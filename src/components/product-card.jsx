ac1b05f525bef2d68976993aa326d14d1f4f621b
Daniel Pliego <danielpliego@gmail.com>
2022-06-07 14:06:21 -0500

    remover descuento

--- src/components/product-card.jsx
+++ src/components/product-card.jsx
@@ -70,12 +70,7 @@ export function ProductCard({ product, eager }) {
             {title}
           </p>
           <div className="product-card-price">
-          {product.productType == 'Hoodies' ? <>
-              <span style={{ textDecoration: 'line-through', opacity: 0.5, marginRight: '1rem' }}>{price}</span>
-              <span style={{ textDecoration: 'line-through' }}> MX $525</span>
-              </> : (
-                <span>{price}</span>
-              )}
+            <span>{price}</span>
           </div>
         </div>
       </Link>
